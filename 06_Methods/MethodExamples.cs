fa3d4853bee8f9395c54379be111c64ddce88703
Jade Neff <njade79162@gmail.com>
2021-09-25 16:48:32 -0400

    end of day lecture notes

---
+++ 06_Methods/MethodExamples.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _06_Methods
+{
+    [TestClass]
+    public class MethodExamples
+    {
+        [TestMethod]
+        public void MethodExecution()
+        {
+            SayHello("Severa");
+            int methodReturn = AddTwoNumbers(1, 2);
+            Console.WriteLine(methodReturn);
+
+            double methodDouble = 3.05;
+            double methodReturnTwo = AddTwoNumbers(methodDouble, methodReturn);
+            Console.WriteLine(methodReturnTwo);
+            int age = CalculateAge(new DateTime(1892, 11, 24));
+            Console.WriteLine($"My age is {age}");
+        }
+        
+        // 1) Access Modifier
+        // 2) Return Type
+        // 3) Method Signature
+            // Name
+            // Parameters
+        // Body - the actual code
+        [TestMethod]
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Hello World! And Hello {name}!");
+        }
+
+        [TestMethod]
+        public int AddTwoNumbers(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+
+        [TestMethod]
+        public double AddTwoNumbers(double numOne, double numTwo)
+        {
+            double sum = numOne + numTwo;
+            return sum;
+        }
+
+        [TestMethod]
+        public int CalculateAge(DateTime birthday)
+        {
+            TimeSpan ageSpan = DateTime.Now - birthday;
+            double totalAgeInYears = ageSpan.TotalDays / 365.25;
+            int years = Convert.ToInt32(Math.Floor(totalAgeInYears));
+            return years;
+        }
+    }
+}
---
+++ 06_Methods/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("06_Methods")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06_Methods")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64425452-4cd8-4a38-a0ed-8ff19c917073")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
