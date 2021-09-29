1762dc1eaf4196b431166165b8b5641d317153d9
Jade Neff <njade79162@gmail.com>
2021-09-29 19:22:08 -0400

    9-29-2021 Challenge

---
+++ 07_Classes/ClassExamples.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Classes
+{
+    public enum VehicleType { Car, Truck, SUV, Minivan, Coupe, Boat }
+
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Mileage { get; set; }
+
+        public VehicleType Type { get; set; }
+        public Vehicle()
+        {
+
+        }
+
+        public Vehicle(string make, string model, int year, double mileage, string color, VehicleType type)
+        {
+            Make = make;
+            Year = year;
+            Model = model;
+            Color = color;
+            Mileage = mileage;
+            Type = type;
+        }
+    }
+
+    public class Person
+    {
+        // age
+        // height
+        // gender
+        // weight
+        // DateOfBirth
+        // firstName
+        // LastName
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int AgeInYears
+        {
+            get
+            {
+                TimeSpan ageSpan = DateTime.Now - DateOfBirth;
+                double totalAgeInYears = ageSpan.TotalDays / 365.25;
+                return Convert.ToInt32(Math.Floor(totalAgeInYears));
+            } 
+        }
+
+        public Vehicle Transport { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName,DateTime dateOfBirth, Vehicle transport)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Transport = transport;
+        }
+    }
+}
---
+++ 07_Classes/PersonRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Classes
+{
+    class PersonRepository
+    {
+        List<Person> personList = new List<Person>();
+
+        public void AddPerson(Person person)
+        {
+            personList.Add(person);
+        }
+
+        public List<Person> ReturnList()
+        {
+            return personList;
+        }
+
+        public void RemovePerson(Person person)
+        {
+            personList.Remove(person);
+        }
+    }
+}
